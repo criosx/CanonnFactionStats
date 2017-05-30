399bf3333eb8fb491ea04630ed8a5f690435042b
criosx <mail@frank-heinrich.net>
2017-05-29 21:35:39 -0400

    first version of parser for populated systems and faction json files from EDDB.io implemented

--- EDDBpull.py
+++ EDDBpull.py
@@ -1,20 +1,60 @@
 import pandas as pd
 
 
-def fn_pull_data_from_eddb():
-    systems_populated = pd.read_json('https://eddb.io/archive/v5/systems_populated.json')
-    factions = pd.read_json('https://eddb.io/archive/v5/factions.json')
+class FactionStats:
+    def __init__(self):
+        pass
 
-    # Canonn faction ID is 14271
+    def fn_pull_data_from_eddb(self, target_id=14271):
+        # Canonn faction ID is 14271
 
-    systems_populated.set_index("name", inplace=True)
-    system_names_canonn = []
-    for system in systems_populated.index:
-        # factions presence is list of dictionaries
-        factions_present = systems_populated.loc[system, 'minor_factions_presences']
-        for faction in factions_present:
-            if faction['minor_faction_id'] == 14271:
-                system_names_canonn.append(system)
+        # systems_populated = pd.read_json('https://eddb.io/archive/v5/systems_populated.json')
+        systems_populated = pd.read_json('~/Desktop/systems_populated.json')
+        # factions = pd.read_json('https://eddb.io/archive/v5/factions.json')
+        factions = pd.read_json('~/Desktop/factions.json')
 
-    systems_canonn = systems_populated.loc[system_names_canonn]
+        # setup dataframes for extracting systems in which Canonn is present and
+        # reduce the faction dataframe to entries only for factions that are in Canonn space
+
+        systems_populated.set_index("name", inplace=True)
+        factions.set_index('id', inplace=True)
+        system_names_target = []
+        faction_names_target = [target_id]
+
+        for system in systems_populated.index:
+
+            # factions presence is list of dictionaries
+            factions_present = systems_populated.loc[system, 'minor_faction_presences']
+            for faction in factions_present:
+
+                # in some systems, the minor_factions_presences entries is not a directory but a list of directories
+                # with possible invalid entries
+                # TODO: follow up on those instances and see if the different format is intentional
+
+                if isinstance(faction, dict):
+                    if faction['minor_faction_id'] == target_id:
+                        system_names_target.append(system)
+                        for otherfaction in factions_present:
+                            if isinstance(otherfaction, dict):
+                                if otherfaction['minor_faction_id'] not in faction_names_target:
+                                    faction_names_target.append(otherfaction['minor_faction_id'])
+                    # else:
+                        # print 'Cannot read data for: ', system
+
+        systems_target = systems_populated.loc[system_names_target]
+        factions_target = factions.loc[faction_names_target]
+
+        return systems_target, factions_target
+
+
+# main program from command line
+
+if __name__ == '__main__':
+
+    facinst = FactionStats()
+    systems, factions = facinst.fn_pull_data_from_eddb()
+    print systems
+    print factions
+    pass
+
 
